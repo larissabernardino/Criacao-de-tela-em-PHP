e0e740b87c66c75b7e8f600239de21ee58a4c8b5
Larissa <bernardinolarissaa@gmai.com>
2022-09-12 14:31:23 -0300

    Index css

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang "en">
     <head>
      <meta charset="UTF-8">
+
      <title>Home Page</title>
+
       <style>
       body , li {
-      /*background-color:black*/
+     
       margin:0;
       padding:0;
       list-style:none;
@@ -13,31 +15,63 @@
       }
       #container{
           display:grid;
-          grid-template-rows:150px auto;
+          grid-template-rows:15vh 85vh;
           grid-template-areas:
           'cabecalho'
           'conteudo'
       }
+      #cabecalho{
+                 grid-area:cabecalho;
+                 background-color:#1d1d1d;
+                 color:white;
+                 display:flex;
+                 justify-content:space-between;
+                 align-items:center;
+                 
+           
+      }
+      #conteudo {
+          grid-area:conteudo;
+          background-color: #ccc;
+      }
+      img {
+          margin-top:15px;
+          margin-left:15px;
+      }
+      .menu{
+          display:flex;
+      }
+      .menu li{
+        padding:20px;
+        margin-left:20px;
+      }
+
+      .menu li a{
+          display:block;
+          padding:10px;
+          cursor:pointer;
+          background-color:green;
+      }
       </style>
      </head>
      <body> 
-  <div id ="container">
-  <header id= "cabeçalho">
-  <div class="logo">
-    <img src="./logo.png">
-</div>        
+  <div id="container">
+  <header id="cabecalho">
+    <div class="logo">
+        <img src="./logo.png"  width="75" height="75">
+        </div>
 <nav>
-    <ul>
-        <li>Home</li>
-        <li>Produtos</li>
-        <li>Clientes</li>
+    <ul class="menu">
+        <li>
+            <a>Home</a></li>
+        <li><a>Produtos</a></li>
+        <li><a>Clientes</a></li>
 </ul>
 </nav>
 </header>
+    <main id = "conteudo">
+    <h1> Bem Vindo a Home</h1>
+    </main>
 </div>
-</head>
-  Bem Vindo a Home
-<body>
-    
 </body>
 </html>
