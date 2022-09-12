56844b7fd9487bcc90bf0037ec9584b0d1f871a9
Larissa <bernardinolarissaa@gmai.com>
2022-09-12 13:52:43 -0300

    css

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<h1>Hello word</h1> 
-<?php
-     echo "Minha Mensagem";
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+     <meta charset="UTF-8">
+     <title>Home Page</title>
+      <style>
+      body , li {
+      /*background-color:black*/
+      margin:0;
+      padding:0;
+      list-style:none;
+      font-family: 'Verdana';
+      }
+      #container{
+          display:grid;
+          grid-template-rows:150px auto;
+          grid-template-areas:
+          'cabecalho'
+          'conteudo'
+      }
+      </style>
+     </head>
+     <body> 
+  <div id ="container">
+  <header id= "cabeçalho">
+  <div class="logo">
+    <img src="./logo.png">
+</div>        
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Produtos</li>
+        <li>Clientes</li>
+</ul>
+</nav>
+</header>
+</div>
+</head>
+  Bem Vindo a Home
+<body>
+    
+</body>
+</html>
